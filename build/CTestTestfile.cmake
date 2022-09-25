d63f0bd7a107be7ddd54b383eb282bb36e9fb25f
Matteo Brentegani <brenteganimatteo@gmail.com>
2022-09-25 11:49:06 +0200

    Revert "added .gitignore"
    This reverts commit 204150de800bff41d28b8e942a78921e24e3ba8e.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/brent/Documents/dev/inventory_test
-# Build directory: C:/Users/brent/Documents/dev/inventory_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,6 @@ void print_menu (Inventory& inv)
 
 void menu_switch (Inventory& inv, char c)
 {
-    std::cin.ignore(1, '\n');
     switch (c)
     {
         case 'S':
